ed815aaedf052d9f257766b4cf7f00757111a1d7
MichaelSRomero <liikemike@aim.com>
2019-01-04 03:25:36 -0500

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,22 @@
 def reformat_languages(languages)
-  # your code here
+  hash_format = {}
+
+  languages.each do |style, language_values|
+    language_values.each do |language, value_hash|
+      type = value_hash.keys[0]
+
+      if hash_format[language] == nil
+        hash_format[language] = {}
+      end
+      hash_format[language][type] = value_hash[:type]
+
+      if hash_format[language][:style] == nil
+        hash_format[language][:style] = [style]
+      else
+        hash_format[language][:style] << style
+      end
+    end
+  end
+
+  return hash_format
 end
