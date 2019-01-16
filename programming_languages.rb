e89d07248fef4e26022ef491248978b2f03bf688
github username <github email address>
2019-01-15 22:59:51 -0500

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,19 @@
 def reformat_languages(languages)
-  # your code here
+  # sets up empty hash
+  new_hash = {}
+  # foe each language first iteration
+  languages.each do |l_style, l_values|
+    # for the values of each language iterate again
+    l_values.each do |type, value|
+      # returns true if given key is equal to type
+      if new_hash.key?(type)
+        # add the style of that l_value into new hash array
+        new_hash[type][:style] << l_style
+      else
+        # adds contents of hashes 
+        new_hash[type] = value.merge!({:style => [l_style]})
+      end
+    end
+  end
+  new_hash
 end
