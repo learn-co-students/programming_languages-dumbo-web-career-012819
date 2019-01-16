cc104610ec7288e4b32e3c28bafc7b2f6bba7860
eselbeus <eselbeus@gmail.com>
2019-01-16 02:46:35 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -11,7 +11,6 @@ def reformat_languages(languages)
         end
         if new_hash[language][:style] == []
           new_hash[language] = {:type => value, :style => [style]}
-          # binding.pry
         else
           new_hash[language][:type => value]
           new_hash[language][:style] << style
