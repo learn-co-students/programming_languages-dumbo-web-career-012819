4cd0243585f27cbde8b6af893046d41d78713c8b
Yong Nam Kang <j.yongnamkang@gmail.com>
2019-01-29 19:42:51 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,17 @@
+require "pry"
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |k, v|
+    v.each do |lang, type|
+      new_hash[lang] = type
+      new_hash[lang][:style] = []
+    end
+  end
+  languages.each do |k, v|
+    v.each do |lang, type|
+      new_hash[lang][:style] << :oo if k == :oo
+      new_hash[lang][:style] << :functional if k == :functional
+    end
+  end
+  new_hash
 end
