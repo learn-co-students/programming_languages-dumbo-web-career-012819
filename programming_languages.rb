a5f65511d9bb4d94427f7c10fb685540125a17d7
Tracy Ho <tracyho0867@gmail.com>
2019-01-30 04:36:52 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -6,23 +6,21 @@ def reformat_languages(languages)
   #   :ruby => {
   #     :type => "interpreted"
   #   },
-  # style_hash = {}
   languages.each do |style, language_hash|
-    language_hash.each do |head, body_hash|
-      new_hash[head] = body_hash
-      new_hash[head][:style] = []
-      # new_hash[head][:style] = style_hash
+    language_hash.each do |language, data|
+      if new_hash.key?(language) == false
+        new_hash[language] = data
+        new_hash[language][:style] = [style]
+      else
+        new_hash[language][:style] << style
+      end
     end
   end
 
-  languages.each do |style, language_hash|
-    language_hash.each do |head, body_hash|
-      # new_hash[head][:style] << style if style == :oo
-      new_hash[head][:style] << style
-    end
-  end
-
-
-  # binding.pry
+  # languages.each do |style, language_hash|
+  #   language_hash.each do |head, body_hash|
+  #     new_hash[head][:style] << style
+  #   end
+  # end
   new_hash
 end
